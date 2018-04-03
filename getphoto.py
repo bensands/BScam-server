5414277edee3f71e450f312c15e28c066439a9bd
benny <bfs43@cornell.edu>
2018-04-03 12:58:00 +0000

    store credentials in separate file

--- getphoto.py
+++ getphoto.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 
 import time
 import paho.mqtt.client as mqtt
@@ -6,15 +6,23 @@ import paho.mqtt.client as mqtt
 # message callback, write image to file
 def on_message(mosq, obj, msg):
 	print("on_message got called")
-	with open('out.jpg', 'wb') as fd:
+	with open('/bem/out.jpg', 'wb') as fd:
 		fd.write(msg.payload)
 
 # set up MQTT client
+print "getphoto.py running at ", time.asctime(time.localtime(time.time()))
+# get server ip:
+with open("credentials.txt","r") as f:
+	serveripcred = f.readline().rstrip()
+
 client = mqtt.Mosquitto("image-rec")
-client.connect("server.ip.goes.here")	# paste in the IP of the MQTT server (this device)
+client.connect(serveripcred)	# paste in the IP of the MQTT server (this device)
 client.subscribe("photo",0)
 client.on_message = on_message
 
-while True:
-	client.loop(15)
-	time.sleep(2)
+client.loop_forever()
+print("getphoto.py code should never get here")
+
+#while True:
+#	client.loop(15)
+#	time.sleep(2)
--- home.py
+++ home.py
@@ -1,5 +1,4 @@
-
-#!/usr/bin/python
+#!/usr/bin/python -u
 
 from bottle import route, run, template, get, post, request, static_file
 import paho.mqtt.client as mqtt
@@ -7,29 +6,37 @@ import time
 
 def on_publish(mosq, userdata, mid):
 	mosq.disconnect()
-
+print "home.py running at ", time.asctime(time.localtime(time.time()))
 client = mqtt.Client()
 client.on_publish = on_publish
 
 @route('/')
 def index():
-	return template("index.html")
+	print("serving index.html...")
+	return template("/bem/index.html")
 
 @route('/<filename>')
 def server_static(filename):
-	return static_file(filename, root='./')
+	return static_file(filename, root='/bem/')
 
 @post('/takepic')
 def takepic():
 	username = request.forms.get('username')
 	password = request.forms.get('password')
-	if username.lower() == 'USERNAME' and password == 'PASS':	# paste in the actual login credentials
-		client.connect('server ip')		# paste in the IP of MQTT server (this device)
+	if username.lower() == usernamecred and password == passwordcred:	# paste in the actual login credentials
+		client.connect(serveripcred)		# paste in the IP of MQTT server (this device)
 		client.publish("takepic", "1")
 		time.sleep(5)							# Time delay so we dont serve up old pics
-		return template("pic.html")
+		return template("/bem/pic.html")
 	else:
 		return "<p>Login failed.</p>"
 
+# get credentials
+with open("credentials.txt","r") as f:
+	serveripcred = f.readline().rstrip()
+	usernamecred = f.readline().rstrip()
+	passwordcred = f.readline().rstrip()
+
 # run command blocks everything below it
+print("about to execute run()")
 run(host = '0.0.0.0', port = 80)
